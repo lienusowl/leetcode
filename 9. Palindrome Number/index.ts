202c51f306f758b1097cf7f1eb5c09aece87d7e0
Никифоров Александр <lienusowl@yandex.ru>
2022-09-10 09:33:01 +0300

    является ли число палиндромом

---
+++ 9. Palindrome Number/index.ts
@@ -0,0 +1,8 @@
+function isPalindrome(x: number): boolean {
+    return x >= 0 && String(x) === String(x).split("").reverse().join("");
+};
+
+console.log(isPalindrome(0));
+console.log(isPalindrome(12344321));
+console.log(isPalindrome(22222));
+console.log(isPalindrome(1234));
